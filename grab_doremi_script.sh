b80d7e0d3e0492f40342d4c09cc205c12c3e35ca
John Rothfield <rthfield@sonic.net>
2012-03-05 22:42:47 -0800

    reorg

--- grab_doremi_script.sh
+++ grab_doremi_script.sh
@@ -1,2 +1,2 @@
-rm -rf public/js/from_doremi_script_base
-cp -r /home/john/doremi-script-base/lib/doremi_script_base public/js/from_doremi_script_base
+#rm -rf public/js/from_doremi_script_base
+#cp -r /home/john/doremi-script-base/lib/doremi_script_base public/js/from_doremi_script_base
