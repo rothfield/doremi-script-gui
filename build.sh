ddaa6e64133d1c77dc777a6190a82537ac3a3b2d
John Rothfield <rthfield@sonic.net>
2012-03-06 17:30:40 -0800

    DRYED up help. Now build.sh will compile the help into readme.html in project root, with a copy in public. The main page also includes the readme as a template

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 cd src/
 haml -r "./render_file.rb" layout.haml > ../public/index.html
+haml -r "./render_file.rb" readme_layout.haml > ../readme.html
+cp ../readme.html ../public/
 cd -
 coffee -o ./public/js -b -c src/*.coffee
